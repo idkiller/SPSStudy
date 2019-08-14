ee791f7d0fe9eec443e860b5705b8370ecddaf58
pius lee <pius.lee@samsung.com>
2019-08-14 19:21:57 +0900

    add user.cpp to problem7

---
+++ problem7/user.cpp
@@ -0,0 +1,32 @@
+typedef struct
+{
+	int y, x;
+}AXIS;
+
+
+void init(int mScreenSize)
+{
+
+}
+void create_memo(int mId, int mY, int mX, int mHeight, int mWidth, char str[])
+{
+
+}
+AXIS select_memo(int mId)
+{
+	AXIS ret;
+	ret.y = ret.x = 0;
+	return ret;
+}
+void move_memo(int mId, int mY, int mX)
+{
+
+}
+void modify_memo(int mId, int mHeight, int mWidth, char str[])
+{
+
+}
+void get_screen_context(int mY, int mX, char res[5][5])
+{
+
+}
